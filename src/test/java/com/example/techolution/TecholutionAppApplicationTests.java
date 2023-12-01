fa17628a7bac079d61b1f92f8852f4f8ecc75d48
pankaj kumar <pankajkumar130697@gmail.com>
2023-12-02 03:29:21 +0530

    setup new spring boot project

---
+++ src/test/java/com/example/techolution/TecholutionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecholutionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
